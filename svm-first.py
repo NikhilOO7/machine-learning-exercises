7800c2f4d160e1f0aa5cbce4f46068871e4b3f88
NikhilOO7 <NikhilOO7@users.noreply.github.com>
2017-11-05 18:50:36 +0530

    Clustering algos

--- svm-first.py
+++ svm-first.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from sklearn import preprocessing, cross_validation, neighbors
+from sklearn import preprocessing, cross_validation, neighbors, svm
 
 import pandas as pd
 
@@ -18,7 +18,7 @@ y = np.array(df['class'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
 
-clf = neighbors.KNeighborsClassifier()
+clf = svm.SVC()
 
 clf.fit(X_train, y_train)
 
